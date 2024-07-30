acea76bb80e661eb24520803883a0ecbfe0582f2
João Carlos Xavier Santos <joao.carlos@conciliadora.local>
2024-07-30 13:57:11 -0300

    alterações na requisição de last

--- api/last.js
+++ api/last.js
@@ -1,20 +1,22 @@
 import fetch from 'node-fetch';
 
-const apiUrl = 'https://api.football-data.org/v4/competitions/BSA/matches';
+const apiUrl = 'https://api.football-data.org/v4/competitions/BSA/matches?dateFrom=2024-07-21&dateTo=2024-07-23&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED';
 const apiKey = '0375969d79f74b60a0a9d73904aa1ee1';
 
 export async function getLast3DaysMatches(req, res) {
     const dateFrom = '2024-07-21';
     const dateTo = '2024-07-23';
+    const season = '2024';
+    const status = 'TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED';
 
     try {
-        const response = await fetch(`${apiUrl}?dateFrom=${dateFrom}&dateTo=${dateTo}&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED`, {
+        const response = await fetch(apiUrl, {
             headers: {
                 'X-Auth-Token': apiKey
             }
         });
         const data = await response.json();
-        res.json(data);
+        res.status(200).json(data);
     } catch (error) {
         console.error('Error fetching last 3 days matches:', error);
         res.status(500).json({ error: 'Internal Server Error' });
