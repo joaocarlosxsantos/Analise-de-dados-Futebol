4ce834f4b85ac5b3b798e01abeae26d82a7b29f7
João Carlos <joaocxs10@gmail.com>
2024-07-31 02:27:52 -0300

    alterando current matchday

--- api/today.js
+++ api/today.js
@@ -1,10 +1,6 @@
 import fetch from 'node-fetch';
-import { getCurrentMatchday  } from './api/rodadaatual.js';
 
-
-const currentMatchday = await getCurrentMatchday(req, res);
-
-const apiUrl = `https://api.football-data.org/v4/competitions/BSA/matches?matchday=${currentMatchday}&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED`;
+const apiUrl = `https://api.football-data.org/v4/competitions/BSA/matches?matchday=20&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED`;
 const apiKey = '0375969d79f74b60a0a9d73904aa1ee1';
 
 export default async function (req, res) {
