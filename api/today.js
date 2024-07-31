31cc31f3d10f001f80923a4cbb504ea9da55030b
João Carlos <joaocxs10@gmail.com>
2024-07-31 00:33:08 -0300

    alterando consulta today com data automatica

--- api/today.js
+++ api/today.js
@@ -1,7 +1,21 @@
 import fetch from 'node-fetch';
 
-const apiUrl = 'https://api.football-data.org/v4/competitions/BSA/matches?dateFrom=2024-07-24&dateTo=2024-07-24&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED';
 const apiKey = '0375969d79f74b60a0a9d73904aa1ee1';
+// Função para formatar a data no formato YYYY-MM-DD
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+// Obter a data atual
+const today = new Date();
+const todayFormatted = formatDate(today);
+
+// Construir a URL da API com a data atual
+const apiUrl = `https://api.football-data.org/v4/competitions/BSA/matches?dateFrom=${todayFormatted}&dateTo=${todayFormatted}&season=2024&status=TIMED,SCHEDULED,LIVE,IN_PLAY,PAUSED,FINISHED`;
+
 
 export default async function (req, res) {
     try {
